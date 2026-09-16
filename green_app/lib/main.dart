2310a9052aedf20f5966e7faa8d2083881fe33f6
Gaju Sandra <gajusandra872@gmail.com>
2026-09-16 09:06:11 +0200

    building on the app

--- green_app/lib/main.dart
+++ green_app/lib/main.dart
@@ -7,10 +7,33 @@ void main() {
 class GreenLifeApp extends StatelessWidget {
   const GreenLifeApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(body: Center(child: Text('Green Life'))),
-    );
-  }
+ @override
+Widget build(BuildContext context) {
+  String appTitle = ' The Green Life';
+
+  return MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title:Center(child:Text(appTitle, style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold))),
+        backgroundColor: const Color.fromARGB(255, 7, 148, 12),
+        elevation: 4,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text('Home', style: TextStyle(color: Colors.white))
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text('About', style: TextStyle(color: Colors.white))
+            ),
+            TextButton(onPressed: () {}, 
+            child: Text('Contact', style: TextStyle(color: Colors.white))),
+        ],
+      ),
+      body: Center(
+        child: Text(appTitle),
+      ),
+    ),
+  );
+}
 }
